3bb246c0d414cbe6bf396908b35e60fbb511b0e1
jonathanjones6fe <jonathanjones6fe+@users.noreply.github.com>
2019-02-10 15:59:48 -0400

    implemented linear search

--- search.cpp
+++ search.cpp
@@ -14,56 +14,57 @@ using namespace std;
 */
 int linearSearch(auto data, auto key);//prototype
 
+int main() {
+	vector<string> inputs;
+	string search_key, input;
+	int result;
+	
+	cout<<"Welcome to \"search it\". We first need some input data."<<endl;
+    cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
+    cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
 
-int main()
-{
-  vector<string> inputs;
-  string search_key, input;
-  int result;
-
-   cout<<"Welcome to \"search it\". We first need some input data."<<endl;
-   cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
-   cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
-
-   cin>>input;
+    cin>>input;
  
-    while(input != "#")//read an unknown number of inputs from keyboard
-    {
-       inputs.push_back(input);
-       cin>>input;
+    while(input != "#") { //read an unknown number of inputs from keyboard
+	    inputs.push_back(input);
+    	cin>>input;
     }
 
-   cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
+    cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
 
-  if(inputs.size() == 0)//no input
-  {
-      cout<<endl<<"No input received, quiting..."<<endl<<endl;
-       exit(1);//nothing to do but quit program
-  }
+    if(inputs.size() == 0) {//no input 
+    	cout<<endl<<"No input received, quiting..."<<endl<<endl;
+    	exit(1);//nothing to do but quit program
+  	}
  
-   cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
-  cout<<"Enter a value to search for: ";
+    cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+    cout<<"Enter a value to search for: ";
 
-
-   cin>>search_key;
+	cin>>search_key;
  
-    while(search_key != "#")//perform searches until sentinel entered
-    {
-        result = linearSearch(inputs,search_key);
+    while(search_key != "#") { //perform searches until sentinel entered
+    	result = linearSearch(inputs,search_key);
 
         cout<<"  '"<<search_key<<"' was ";
 
         if (result == -1)
-          cout<<"not found";
+        	cout<<"not found";
         else
-          cout<<"found at index "<<result;
+        	cout<<"found at index "<<result;
 
 
         cout<<endl<<endl<<"Enter a value to search for: ";
         cin>>search_key; 
     }
 
-   cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
+    cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
 
     return 0;
+}
+
+int linearSearch(auto data, auto key) {
+	for(unsigned int i = 0; i < data.size(); i++)
+		if (data[i] == key) 
+			return i;
+	return -1; //if u made it this far not found
 }
